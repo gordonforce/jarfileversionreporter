df7666c56cfb0fe21e0bedd8a04147db330f9ef8
Gordon Force Jr <gordonff@gmail.com>
2023-02-08 16:17:47 +0000

    Added more tests and source file jar.

--- src/test/java/com/github/gordonfforce/jarfileversionreporter/jar/DefaultJarOperationsImplTest.java
+++ src/test/java/com/github/gordonfforce/jarfileversionreporter/jar/DefaultJarOperationsImplTest.java
@@ -7,11 +7,20 @@ import static org.mockito.Mockito.when;
 import com.github.gordonfforce.jarfileversionreporter.filefilter.FileMetaDataFilter;
 import com.github.gordonfforce.jarfileversionreporter.jar.JarOperationsFactory.DefaultJarOperationsImpl;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class DefaultJarOperationsImplTest {
 
@@ -20,10 +29,60 @@ class DefaultJarOperationsImplTest {
 
   final FileMetaDataFilter fileFilter = mock(FileMetaDataFilter.class);
 
-
   final Path basePath = mock(Path.class);
 
   final JarOperations jarOps = new DefaultJarOperationsImpl(fileFilter);
+
+  private static final Path A_JAR_FILE_PATH = Paths.get("./tmp/test.jar");
+
+  @BeforeAll
+  static void beforeAll() {
+
+    Optional.of(A_JAR_FILE_PATH.getParent())
+        .filter(path -> !Files.exists(path))
+        .ifPresent(path -> {
+          try {
+            Files.createDirectories(path);
+          } catch (IOException e) {
+            throw new IllegalStateException("Could not create directory", e);
+          }
+        });
+
+    Optional.of(A_JAR_FILE_PATH)
+        .filter(path -> !Files.exists(path))
+        .ifPresent(path -> {
+          try {
+            Files.createFile(path);
+          } catch (IOException e) {
+            throw new IllegalStateException("Could not create file", e);
+          }
+        });
+  }
+
+  @AfterAll
+  static void afterAll() {
+
+    Optional.of(A_JAR_FILE_PATH)
+        .filter(Files::exists)
+        .ifPresent(path -> {
+          try {
+            Files.delete(path);
+          } catch (IOException e) {
+            throw new IllegalStateException("Could not delete file", e);
+          }
+        });
+
+    Optional.of(A_JAR_FILE_PATH.getParent())
+        .filter(Files::exists)
+        .ifPresent(path -> {
+          try {
+            Files.delete(path);
+          } catch (IOException e) {
+            throw new IllegalStateException("Could not delete directory", e);
+          }
+        });
+
+  }
 
   @BeforeEach
   void setUp() throws Exception {
@@ -46,6 +105,7 @@ class DefaultJarOperationsImplTest {
         .thenReturn(Stream.of(thisJarFilePath, thatJarFilePath))
         .thenReturn(Stream.of(thisJarFilePath, thatJarFilePath));
 
+
   }
 
   @Test
@@ -66,10 +126,23 @@ class DefaultJarOperationsImplTest {
 
   }
 
-  @Test
-  void filter() {
-    // TODO
-    assertThat(true).isTrue();
+  private static Stream<Arguments> provideFilter() {
+
+    return Stream.of(
+        Arguments.of(A_JAR_FILE_PATH.getParent(), A_JAR_FILE_PATH, Collections.emptyList()));
+
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideFilter")
+  void filter(
+      final Path basePath,
+      final Path jarFilePath,
+      final Collection<String> expected)  {
+
+    assertThat(jarOps.filter(basePath, jarFilePath)).isEqualTo(expected);
+
+
   }
 
 }
